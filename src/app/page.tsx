78eca19152045bfa99e31842af010cc0b1a18276
Vincent Mukuna <mukunavincent28@gmail.com>
2024-10-20 20:39:18 +0300

    feat: testimonials block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { CtaBlock } from "./_components/cta-block";
 import { HeroSection } from "./_components/hero-section";
 import { ProjectShowcaseBlock } from "./_components/project-showcase-block";
 import { SolutionsBlock } from "./_components/solutions-block";
+import { TestimonialsBlock } from "./_components/testimonials-block";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection />
       <ProjectShowcaseBlock />
       <SolutionsBlock />
+      <TestimonialsBlock />
       <CtaBlock />
     </main>
   );
