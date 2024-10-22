0cdb3253d54dd91fd99c1da02564710d58048a77
Vincent Mukuna <mukunavincent28@gmail.com>
2024-10-23 02:37:28 +0300

    chore: update home page cta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
 
 import { CtaBlock } from "../components/cta-block";
 import { TestimonialsBlock } from "../components/testimonials-block";
@@ -17,12 +21,21 @@ export default function Home() {
         <CtaBlock.Header>
           <CtaBlock.Title>Let&apos;s Connect and Collaborate!</CtaBlock.Title>
           <CtaBlock.Description>
-            Follow me on social media or reach out directly.
+            I’m always eager to connect with like-minded professionals and
+            explore new opportunities. Whether you have a project in mind or
+            simply want to discuss ideas, I&apos;m here to collaborate and bring
+            your vision to life. Let’s harness our skills to create innovative
+            solutions together!
           </CtaBlock.Description>
         </CtaBlock.Header>
         <CtaBlock.Actions>
-          <Button variant="default">Contact Us</Button>
-          <Button variant="outline">Follow</Button>
+          <Link
+            href={siteConfig.links.upwork}
+            target={"_blank"}
+            className={cn(buttonVariants(), "inline-flex gap-1")}
+          >
+            Hire Me!
+          </Link>
         </CtaBlock.Actions>
       </CtaBlock>
     </main>
