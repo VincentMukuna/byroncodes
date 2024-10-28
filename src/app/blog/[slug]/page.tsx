864fc936e45966a28cea582bb944055104544d57
Vincent Mukuna <mukunavincent28@gmail.com>
2024-10-28 20:40:14 +0300

    chore: log post metadata

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,7 @@
+import { Metadata } from "next";
+
+import { siteConfig } from "@/config/site";
+
 import { BlogPostContent } from "./_components/blog-post-content";
 import { BlogPostCta } from "./_components/blog-post-cta";
 import { BlogPostHeader } from "./_components/blog-post-header";
@@ -13,3 +17,39 @@ export default function BlogPostPage() {
     </main>
   );
 }
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description:
+    "Read my latest blog posts on fullstack development, coding tutorials, and insights on software development.",
+  openGraph: {
+    title: "Blog",
+    siteName: siteConfig.name,
+    url: `${siteConfig.url}/blog`,
+    description:
+      "Read my latest blog posts on fullstack development, coding tutorials, and insights on software development.",
+    images: [
+      {
+        url: new URL("/img/blog.jpg", siteConfig.url).toString(),
+        width: 1200,
+        height: 630,
+        alt: "Blog",
+      },
+    ],
+  },
+  twitter: {
+    site: "@mandela_byron",
+    title: "Blog",
+    card: "summary_large_image",
+    description:
+      "Read my latest blog posts on fullstack development, coding tutorials, and insights on software development.",
+    images: [
+      {
+        url: new URL("/img/blog.jpg", siteConfig.url).toString(),
+        width: 1200,
+        height: 630,
+        alt: "Blog",
+      },
+    ],
+  },
+};
