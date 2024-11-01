d8b28c0e814d227fcb4a71392ac79f5f0c5f4d6f
Vincent Mukuna <mukunavincent28@gmail.com>
2024-11-02 00:23:07 +0300

    chore: set up payloadcms

---
+++ src/payload.config.ts
@@ -0,0 +1,39 @@
+// storage-adapter-import-placeholder
+import { postgresAdapter } from "@payloadcms/db-postgres";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import path from "path";
+import { buildConfig } from "payload";
+import sharp from "sharp";
+import { fileURLToPath } from "url";
+
+import { env } from "./env/server";
+import { Media } from "./payload/collections/Media";
+import { Users } from "./payload/collections/Users";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+    avatar: "gravatar",
+  },
+  collections: [Users, Media],
+  editor: lexicalEditor(),
+  secret: env.PAYLOAD_SECRET || "",
+  typescript: {
+    outputFile: path.resolve(dirname, "payload-types.ts"),
+  },
+  db: postgresAdapter({
+    pool: {
+      connectionString: env.DATABASE_URI || "",
+    },
+  }),
+  sharp,
+  plugins: [
+    // storage-adapter-placeholder
+  ],
+});
