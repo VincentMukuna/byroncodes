90ea258825b26b00312db356110cfe0790b2abb2
Vincent Mukuna <mukunavincent28@gmail.com>
2024-11-07 19:06:04 +0300

    fiix:types

--- src/payload/blocks/Content/index.ts
+++ src/payload/blocks/Content/index.ts
@@ -54,7 +54,8 @@ const columnFields: Field[] = [
   link({
     overrides: {
       admin: {
-        condition: (_, { enableLink }) => Boolean(enableLink),
+        condition: (_: unknown, { enableLink }: { enableLink: boolean }) =>
+          Boolean(enableLink),
       },
     },
   }),
