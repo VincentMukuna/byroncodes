a32fe11d9b0f81580d1af2def084af35fddcde89
VincentMukuna <mukunavincent28@gmail.com>
2024-12-10 19:41:02 +0300

    fix: formatting

--- src/components/blocks/Form/index.tsx
+++ src/components/blocks/Form/index.tsx
@@ -18,6 +18,12 @@ import { fields } from "./fields";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-nocheck
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
+
 export type Value = unknown;
 
 export interface Property {
