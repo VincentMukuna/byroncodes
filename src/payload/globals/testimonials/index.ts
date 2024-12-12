33d716360ad90944dab8ac6834121beca9139d69
VincentMukuna <mukunavincent28@gmail.com>
2024-12-12 03:41:05 +0300

    feat: restructure testimonials fields to include role and collapse admin section

--- src/payload/globals/testimonials/index.ts
+++ src/payload/globals/testimonials/index.ts
@@ -10,25 +10,33 @@ export const Testimonials: GlobalConfig = {
       type: "array",
       label: "Testimonials",
       required: true,
+      admin: { initCollapsed: true },
       fields: [
         {
-          name: "name",
-          label: "Name",
-          type: "text",
-          required: true,
+          type: "row",
+          fields: [
+            {
+              name: "name",
+              label: "Name",
+              type: "text",
+              required: true,
+            },
+            {
+              name: "role",
+              type: "text",
+              label: "Role",
+              required: true,
+            },
+          ],
         },
+
         {
           name: "quote",
           label: "Quote",
           type: "textarea",
           required: true,
         },
-        {
-          name: "role",
-          type: "text",
-          label: "Role",
-          required: true,
-        },
+
         {
           name: "rating",
           type: "number",
