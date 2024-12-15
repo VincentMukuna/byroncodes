862f7741d020eb941768328a49e1a2a074f9362e
VincentMukuna <mukunavincent28@gmail.com>
2024-12-15 16:45:47 +0300

    feat: move Google site verification to metadata object for better organization

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -35,12 +35,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <meta
-          name="google-site-verification"
-          content="hcnxp2hTBwVg5tcytxTUJalSV_rbY_O2YNDD7PU0Kh8"
-        />
-      </head>
       <body
         className={cn(
           robotoFont.variable,
@@ -71,6 +65,9 @@ export const metadata: Metadata = {
   },
   description: siteConfig.description,
   metadataBase: new URL(siteConfig.url),
+  verification: {
+    google: "hcnxp2hTBwVg5tcytxTUJalSV_rbY_O2YNDD7PU0Kh8",
+  },
   keywords: [
     "Fullstack software developer",
     "Python developer",
