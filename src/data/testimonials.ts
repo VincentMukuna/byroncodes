843f68ab3ded4ce64e74612ca08e824ec613ed45
VincentMukuna <mukunavincent28@gmail.com>
2024-12-17 19:20:47 +0300

    feat: add logging for fetching testimonials and limit results to 10

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -5,10 +5,15 @@ import { buildPayloadHMR } from "@/utils/buildPayloadHMR";
 export const getTestimonials = unstable_cache(async () => {
   const payload = await buildPayloadHMR();
 
+  payload.logger.info("Fetching testimonials");
+
   const { docs: testimonials } = await payload.find({
     collection: "testimonials",
     depth: 2,
+    limit: 10,
   });
+
+  payload.logger.info(`Fetched ${testimonials.length} testimonials`);
 
   return testimonials;
 }, ["testimonials"]);
