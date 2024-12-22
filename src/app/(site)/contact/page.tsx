652916a9ec2b11ff7f6dd8fc3a9e11e2638df888
VincentMukuna <mukunavincent28@gmail.com>
2024-12-22 18:32:36 +0300

    feat: enable client-side rendering for ContactForm and comment out metadata

--- src/app/(site)/contact/page.tsx
+++ src/app/(site)/contact/page.tsx
@@ -1,7 +1,6 @@
-import { Metadata } from "next";
-import dynamic from "next/dynamic";
+"use client";
 
-import { siteConfig } from "@/config/site";
+import dynamic from "next/dynamic";
 
 const ContactForm = dynamic(() => import("./_components/contact-form"), {
   ssr: false,
@@ -20,37 +19,37 @@ export default function ContactPage() {
   );
 }
 
-export const metadata: Metadata = {
-  title: "Contact Me",
-  description:
-    "Get in touch with Byron Mandela, a skilled developer. Whether you have a project in mind or just want to connect, reach out through this contact form.",
-  openGraph: {
-    title: "Contact Me",
-    description:
-      "Get in touch with Byron Mandela, a skilled developer. Whether you have a project in mind or just want to connect, reach out through this contact form.",
-    type: "website",
-    url: new URL("/contact", siteConfig.url).toString(),
-    images: [
-      {
-        url: new URL("/img/og/opengraph-image.jpeg", siteConfig.url).toString(),
-        width: 1200,
-        height: 630,
-        alt: "ByronCodes",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Contact Me",
-    description:
-      "Get in touch with Byron Mandela, a skilled developer. Whether you have a project in mind or just want to connect, reach out through this contact form.",
-    images: [
-      {
-        url: new URL("/img/og/opengraph-image.jpeg", siteConfig.url).toString(),
-        width: 1200,
-        height: 630,
-        alt: "ByronCodes",
-      },
-    ],
-  },
-};
+// export const metadata: Metadata = {
+//   title: "Contact Me",
+//   description:
+//     "Get in touch with Byron Mandela, a skilled developer. Whether you have a project in mind or just want to connect, reach out through this contact form.",
+//   openGraph: {
+//     title: "Contact Me",
+//     description:
+//       "Get in touch with Byron Mandela, a skilled developer. Whether you have a project in mind or just want to connect, reach out through this contact form.",
+//     type: "website",
+//     url: new URL("/contact", siteConfig.url).toString(),
+//     images: [
+//       {
+//         url: new URL("/img/og/opengraph-image.jpeg", siteConfig.url).toString(),
+//         width: 1200,
+//         height: 630,
+//         alt: "ByronCodes",
+//       },
+//     ],
+//   },
+//   twitter: {
+//     card: "summary_large_image",
+//     title: "Contact Me",
+//     description:
+//       "Get in touch with Byron Mandela, a skilled developer. Whether you have a project in mind or just want to connect, reach out through this contact form.",
+//     images: [
+//       {
+//         url: new URL("/img/og/opengraph-image.jpeg", siteConfig.url).toString(),
+//         width: 1200,
+//         height: 630,
+//         alt: "ByronCodes",
+//       },
+//     ],
+//   },
+// };
